6db9db172623e6735daa0e6c7209a2a7321c0427
Igor <igorlins54@gmail.com>
2023-10-11 21:24:52 -0300

    config: add model e adm.py

--- lista/models.py
+++ lista/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    item = models.CharField(max_length=200)
+
+
+#essa classe tem uma função que retorna o nome do item {
+# }
+    def __str__(self):
+        return self.item
+
