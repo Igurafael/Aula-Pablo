5cf8de2aaf72eb78714ce1dc5a0153ef01df3681
Igor <igorlins54@gmail.com>
2023-11-01 21:19:44 -0300

    mais coisa

--- lista/urls.py
+++ lista/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import lista
+from . import views
 
 urlpatterns = [
-    path('', lista)
+    path('', views.lista, name='lista'),
+    path('add_item', views.add_item, name='add_item')
 ]
