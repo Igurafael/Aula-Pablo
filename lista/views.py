891a8e2ff16d58391bc3ee3e619e0b3ea8a5d33a
Igor Rafael <99566632+Igurafael@users.noreply.github.com>
2023-12-04 21:57:13 -0300

    agora vai

--- lista/views.py
+++ lista/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 from .forms import ItemForm
 
@@ -8,8 +8,20 @@ def lista(request):
     context = {'itens':itens}
     return render(request, "index.html", context)
 
-def add_item(request): 
-    if request.method == 'GET':
+def add_item(request):
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista')
+    else:
         form = ItemForm()
-        context = {'form':form}
-        return render(request, "add_item.html", context)
+
+    context = {'form': form}
+    return render(request, "add_item.html", context)
+
+# def add_item(request): 
+#     if request.method == 'POST':
+#         form = ItemForm()
+#         context = {'form':form}
+#         return render(request, "add_item.html", context)
